81d69f4bb13b2125c94e24ba74c9c678c23e5eeb
Emmanuel Ugwu <>
2022-11-21 16:28:05 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+
+<script>
+  import CarList from "./CarList.svelte";
+  import CarPage from "./CarPage.svelte";
+</script>
+<main>
+  <h1>Car Dealership</h1>
+  <CarList />
+  <CarPage />
+</main>
---
+++ src/CarPage.svelte
@@ -0,0 +1,12 @@
+
+<script>
+    import cars from "./store/cars";
+</script>
+
+<div id="carPage">
+    { #each $cars as car }
+            <p>{car.make} {car.model}</p>
+            <p>{car.year}</p>
+            <hr />
+    { /each }
+</div>
